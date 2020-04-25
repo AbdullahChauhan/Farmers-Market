13078df6933109e49ccee4801f13f063933866c2
AbdullahChauhan <acse.uok@gmail.com>
2020-04-25 21:34:01 +0500

    cupertinopage routes setup

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -1,6 +1,7 @@
 import 'package:farmers_market/app/screens/landing.dart';
 import 'package:farmers_market/app/screens/login.dart';
 import 'package:farmers_market/app/screens/signup.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 abstract class Routes {
@@ -17,6 +18,23 @@ abstract class Routes {
         break;
       default:
         return MaterialPageRoute(builder: (_) => Login());
+        break;
+    }
+  }
+
+  static CupertinoPageRoute cupertinoRoutes(RouteSettings settings) {
+    switch(settings.name) {
+      case '/':
+        return CupertinoPageRoute(builder: (_) => Landing());
+        break;
+      case '/signup':
+        return CupertinoPageRoute(builder: (_) => SignUp());
+        break;
+      case '/login':
+        return CupertinoPageRoute(builder: (_) => Login());
+        break;
+      default:
+        return CupertinoPageRoute(builder: (_) => Login());
         break;
     }
   }
