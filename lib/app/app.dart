68ff1c4d508125bcc2ede3ca2d0b6df26079077a
AbdullahChauhan <acse.uok@gmail.com>
2020-04-25 20:11:54 +0500

    initial app setup

---
+++ lib/app/app.dart
@@ -0,0 +1,12 @@
+import 'package:farmers_market/app/screens/login.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    );
+  }
+}
