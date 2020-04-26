4521a1ac30214d1690de633676891fc562480e85
AbdullahChauhan <acse.uok@gmail.com>
2020-04-26 17:34:32 +0500

    platform splitting things

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,26 @@
 import 'package:farmers_market/app/routes.dart';
 import 'package:farmers_market/app/screens/login.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return PlatformApp();
+  }
+}
+
+class PlatformApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if(Platform.isIOS) {
+      return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+      onGenerateRoute: Routes.cupertinoRoutes,
+    );
+    }
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Login(),
