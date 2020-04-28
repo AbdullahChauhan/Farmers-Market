59be81afdfea73a6bce447aa66ef2841499e4332
AbdullahChauhan <acse.uok@gmail.com>
2020-04-28 23:11:41 +0500

    login page basic skeleton

--- lib/app/screens/login.dart
+++ lib/app/screens/login.dart
@@ -1,8 +1,27 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    if(Platform.isIOS) {
+      return CupertinoPageScaffold(
+        child: _pageBody(context),
+      );
+    }
+
+    return Scaffold(
+      body: _pageBody(context),
+    );
+  }
+
+  Widget _pageBody(BuildContext context) {
+    return ListView(
+      children: [
+
+      ],
+    );
   }
 }
