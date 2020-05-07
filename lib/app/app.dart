e40642997bcae322694c18ab7cf111599ac762ef
AbdullahChauhan <acse.uok@gmail.com>
2020-05-07 14:40:53 +0500

    injecting the provider on the top - platform app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,29 @@
+import 'package:farmers_market/app/blocs/auth_bloc.dart';
 import 'package:farmers_market/app/routes.dart';
 import 'package:farmers_market/app/screens/login.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
-class App extends StatelessWidget {
+import 'package:provider/provider.dart';
+
+final authBloc = AuthBloc();
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return PlatformApp();
+    return Provider(create: (context) => authBloc, child: PlatformApp());
+  }
+
+  @override
+  void dispose() {
+    authBloc.dispose();
+    super.dispose();
   }
 }
 
