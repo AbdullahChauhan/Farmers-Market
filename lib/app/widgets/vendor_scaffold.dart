aca55ff586897f3aa648053168eee4a7557b4fc5
AbdullahChauhan <acse.uok@gmail.com>
2020-05-14 14:33:55 +0500

    completing the page selection func and injecting it to builder

--- lib/app/widgets/vendor_scaffold.dart
+++ lib/app/widgets/vendor_scaffold.dart
@@ -1,34 +1,40 @@
 import 'package:farmers_market/app/styles/colors.dart';
+import 'package:farmers_market/app/widgets/orders.dart';
+import 'package:farmers_market/app/widgets/products.dart';
+import 'package:farmers_market/app/widgets/profile.dart';
 import 'package:flutter/cupertino.dart';
 
 abstract class VendorScaffold {
   static CupertinoTabScaffold get cupertinoTabScaffold {
     return CupertinoTabScaffold(
       tabBar: _cupertinoTabBar,
-      tabBuilder: (context, index) {
-
-      },
+      tabBuilder: (context, index) => _pageSelection(index),
     );
   }
 
   static get _cupertinoTabBar {
-    return CupertinoTabBar(  
+    return CupertinoTabBar(
       backgroundColor: AppColors.darkBlue,
       items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.create), title: Text('Product')),
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.shopping_cart), title: Text('Orders')),
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.person), title: Text('Profile'))
+        BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.create), title: Text('Product')),
+        BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart), title: Text('Orders')),
+        BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person), title: Text('Profile'))
       ],
     );
   }
 
   static Widget _pageSelection(int index) {
-    if(index == 0) {
-
-    } else if(index == 1) {
-
-    } else {
-
+    if (index == 0) {
+      return Products();
     }
+
+    if (index == 1) {
+      return Orders();
+    }
+
+    return Profile();
   }
 }
