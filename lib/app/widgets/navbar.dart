cf5c66f804b2ad2bb6fb7d0962d3cadeb2244fc5
AbdullahChauhan <acse.uok@gmail.com>
2020-05-15 12:40:59 +0500

    creating sliver appbar for material

--- lib/app/widgets/navbar.dart
+++ lib/app/widgets/navbar.dart
@@ -1,10 +1,20 @@
+import 'package:farmers_market/app/styles/colors.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 abstract class AppNavbar {
 
   static CupertinoSliverNavigationBar cupertinoNavBar ({String title, BuildContext context}) {
     return CupertinoSliverNavigationBar(  
       largeTitle: Text(title),
+    );
+  }
+
+  static SliverAppBar materialNavBar({@required String title, @required TabBar tabBar}) {
+    return SliverAppBar(
+      title: Text(title),
+      backgroundColor: AppColors.darkBlue,
+      bottom: tabBar,
     );
   }
 }
